4f24fabd34b8e4c4cdd78c420b0bbecaeb95da10
Giannis Christofakis <giannis_christofakis@hotmail.com>
2020-05-09 23:21:02 +0300

    add more efficient methods and test cases

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -1,6 +1,62 @@
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
 class Solution {
 
+    // worst-case time complexity is O(N*log(N)) - covered
+    // worst-case space complexity is O(N) - don't think is covered
+    public int countIdenticalPairsWithMapping(int[] array) {
+        Map<Integer,Integer> occurrences = new HashMap<>();
+        for (int i =0; i <array.length;i++) {
+            int element = array[i];
+            occurrences.put( element, occurrences.getOrDefault(element, 0) + 1 );
+        }
+
+        return occurrences.values()
+                .stream().filter(e -> e > 1).map(Solution::combination).reduce(0, Integer::sum);
+    }
+
+    // Mathematical equation
+    public static int combination(final int N) {
+        return (int) ( N * (N - 1 ) * 0.5 ) ;
+    }
+
+    // calculate recursively
+    public static int combinatory(final int N) {
+        if (N == 0 || N == 1) return 0;
+        if (N == 2) return 1;
+        return N - 1 + combinatory(N - 1);
+    }
+
+    //  better, complexity for sorting O(N*log(N))
+    public int countIdenticalPairsWithSorting(int[] array) {
+        Arrays.sort(array);
+
+        int duplicates = 0;
+        int occurrences = 1;
+        for (int i = 0; i + 1 < array.length; i++) {
+            if ( array[i] == array[i + 1] ) occurrences++;
+            else {
+                duplicates += combinatory(occurrences);
+                occurrences = 1;
+            }
+        }
+
+        duplicates += combinatory(occurrences);
+        return duplicates;
+    }
+
     public int countIdenticalPairs(int[] array) {
+        int duplicates = countIdenticalPairsWithMapping(array);
+//        int duplicates = countIdenticalPairsWithSorting(array);
+//        int duplicates = countIdenticalPairsIterative(array);
+
+        return  1_000_000_000 < duplicates ? 1_000_000_000 : duplicates;
+    }
+
+    // not efficient
+    public int countIdenticalPairsIterative(int[] array) {
         int duplicates = 0;
 
         for (int i = 0; i < array.length; i++) {
@@ -11,7 +67,7 @@ class Solution {
             }
         }
 
-        return  1_000_000_000 < duplicates ? 1_000_000_000 : duplicates;
+        return duplicates;
     }
 }
 
--- src/test/java/SolutionTest.java
+++ src/test/java/SolutionTest.java
@@ -1,9 +1,13 @@
-import org.junit.Test;
 import org.junit.Before;
+import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 public class SolutionTest {
+
+    private static final int MAX_IDENTICAL_PAIRS = 1_000_000_000;
+    private static final int MAX_N = 100_000;
 
     private Solution solution;
 
@@ -15,6 +19,60 @@ public class SolutionTest {
     @Test
     public void testExampleData() throws Exception {
         assertEquals(4,solution.countIdenticalPairs(new int[]{3,5,6,3,3,5}));
+    }
+
+    @Test
+    public void zeroDuplicates() throws Exception {
+        assertEquals(0,solution.countIdenticalPairs(new int[]{3,5,6,4,0,9}));
+    }
+
+    @Test
+    public void twoPairsData() throws Exception {
+        assertEquals(5,solution.countIdenticalPairs(new int[]{3,5,6,3,3,5,6}));
+    }
+
+    @Test
+    public void threePairsData() throws Exception {
+        assertEquals(7,solution.countIdenticalPairs(new int[]{3,5,6,3,3,5,6,0,6}));
+    }
+
+    @Test
+    public void threePairsWithQuatroData() throws Exception {
+        assertEquals(10,solution.countIdenticalPairs(new int[]{3,5,6,3,3,5,6,3,6}));
+    }
+
+//    @Test // The combinations can't exceed the maximum number of identical pairs of indices.
+    public void countMaxElementRange() throws Exception {
+        int[] intArray = new int[MAX_N]; // By default the value is 0
+        int identicalPairs = solution.countIdenticalPairs( intArray );
+        assertTrue(identicalPairs < MAX_IDENTICAL_PAIRS);
+    }
+
+
+
+    @Test
+    public void testCombinatory() {
+        assertEquals(10, Solution.combinatory(5));
+    }
+
+    @Test
+    public void threeValueCombinatory() {
+        assertEquals(3, Solution.combinatory(3));
+    }
+
+    @Test
+    public void twoValueCombinatory() {
+        assertEquals(1, Solution.combinatory(2));
+    }
+
+    @Test
+    public void OneValueCombinatory() {
+        assertEquals(0, Solution.combinatory(1));
+    }
+
+    @Test
+    public void zeroValueCombinatory() {
+        assertEquals(0, Solution.combinatory(0));
     }
 }
  
